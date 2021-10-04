9066f6d721c90c9134c880ecb1ce79aa1328f1ba
acerpa7 <91565833+acerpa7@users.noreply.github.com>
2021-10-04 15:03:19 -0500

    Add files via upload

---
+++ alexa.colors.py
@@ -0,0 +1,15 @@
+#Initialize Turtle
+import turtle
+import random
+alexa = turtle.Turtle()
+
+#Functions
+def randomDot():
+    alexa.color("pink")
+    alexa.begin_fill()
+    alexa.circle(100)
+    alexa.end_fill()
+#Main
+randomDot()
+
+
